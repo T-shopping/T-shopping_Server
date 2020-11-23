9f6e2eeee18f9e31a38bd9e97eac7ee75b17ae20
JeongYongWoo <rhkd6303@gmail.com>
2020-11-23 22:40:51 +0900

    [ ADD ]
    Login & Register

--- src/routes/auth/auth.controller.js
+++ src/routes/auth/auth.controller.js
@@ -1,5 +1,18 @@
-exports.reqister = (res,req =>{
-    const user = {
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const dbconfig = require('');
+const dbOptions = dbconfig;
+app.use(session({
+    secret: process.env.secret,
+    store: new MySQLStore(dbOptions),
+    resave: false,
+    saveUninitialized: false
+}))
+
+exports.reqister = (res,req) =>{
+    const User_info = {
         "email" : req.body.email,
         "user_name" : req.body.user_name,
         "address": req.body.address,
@@ -7,5 +20,57 @@ exports.reqister = (res,req =>{
         "phone" : req.body.phone,
         "gender": req.body.gender
     }
-    connenction.query
-}) 
+    connenction.query('INSERT INTO User_info SET ?' , User_info, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred", error);
+            res.send({
+                "code" : 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            console.log('The solution is: ', results);
+            res.send({
+                "code": 200,
+                "success": "user registered sucessfully"
+            });
+        }
+    });
+}
+exports.login = (res, req) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+    connenction.qurey('SELECT * FROM User_info WHERE email = ?',[email]),
+    function(error, results, fields){
+        if (error){
+            res.send({
+                "code" : 400,
+                "failed" : "error ocurred"
+            })
+        } else{
+            if(results.length > 0) {
+                if(results[0].password == password) {
+                    res.send({
+                        "code": 200,
+                        "success": "login sucessfull"
+                    });
+                    req.session.email = results[0].email;
+                    req.session.save(function(){ 
+                        rsp.redirect('/');
+                    });
+                } else {
+                    res.send({
+                        "code": 204,
+                        "success": "Email and password does not match"
+                    });
+                }
+            } else {
+                res.send({
+                    "code":204,
+                    "success": "Email does not exists"
+                });
+            }
+
+
+        }
+    }
+}
