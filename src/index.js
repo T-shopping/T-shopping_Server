dd44f165aedde50372acec607e6fb8fee4bfd200
ksh030506 <llmm030506@naver.com>
2020-11-23 20:22:00 +0800

    [ Add ]
    Route init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+
+const api = require('./routes');
+
+app.use('/', api);
 
 app.get('/', (req, res) => {
   res.send('GET request to the homepage');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
     console.log("3000포트에서 열림");
 });
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const route = require('express').Router();
+
+// const auth = require('./auth');
+// const commity = require('./commity');
+// const shop = require('./shop');
+
+
+module.exports = route;
