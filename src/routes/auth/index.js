8dcbb9ff7b7e826d81963686845fcc89841fac36
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-11-24 11:08:36 +0900

    123

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -3,3 +3,5 @@ const auth = express();
 
 auth.post('/register/local', require('./auth.controller').register);
 auth.post('/login/local', require('./auth.controller').login);
+
+module.exports = auth;
